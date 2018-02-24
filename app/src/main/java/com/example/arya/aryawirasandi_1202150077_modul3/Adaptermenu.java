69b890b8ee1fb2a15417da16a69ef2f2e4eddf5b
aryawirasandi <aryawirasandi7153@gmail.com>
2018-02-24 18:54:07 +0700

    first commit

---
+++ app/src/main/java/com/example/arya/aryawirasandi_1202150077_modul3/Adaptermenu.java
@@ -0,0 +1,9 @@
+package com.example.arya.aryawirasandi_1202150077_modul3;
+
+/**
+ * Created by arya on 24/02/18.
+ */
+
+public class Adaptermenu {
+
+}
---
+++ app/src/main/java/com/example/arya/aryawirasandi_1202150077_modul3/Daftarmenu.java
@@ -0,0 +1,13 @@
+package com.example.arya.aryawirasandi_1202150077_modul3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Daftarmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daftarmenu);
+    }
+}
